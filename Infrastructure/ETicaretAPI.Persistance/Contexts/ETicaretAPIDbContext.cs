d2b45f47b9caab31f122c61c51f2bd7b6d44dfc3
00003688 <kadiraydogdu@tcdd.gov.tr>
2023-04-13 17:13:20 +0300

    güncelleme

--- Infrastructure/ETicaretAPI.Persistance/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistance/Contexts/ETicaretAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -17,5 +18,18 @@ namespace ETicaretAPI.Persistance.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var data in datas) 
+            {
+                var _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                };
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -12,18 +12,28 @@ namespace ETicaretAPI.API.Controllers
     {
         private readonly IProductWriteRepository _productWriteRepository;
         private readonly IProductReadRepository _productReadRepository;
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly ICustomerWriteRepository _customerWriteRepository;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
         }
 
         [HttpGet]   
         public async Task Get()
         {
-            await _productWriteRepository.AddAsync(new() { Name = "C Product", Price = 1.500F, Stock = 10, CreatedDate= DateTime.UtcNow });
-            await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddAsync(new() { Name = "C Product", Price = 1.500F, Stock = 10, CreatedDate= DateTime.UtcNow });
+            //await _productWriteRepository.SaveAsync();
+            var customerId = Guid.NewGuid();
+            await _customerWriteRepository.AddAsync(new() {Id=customerId, Name="Kadir"});
+            await _orderWriteRepository.AddAsync(new() { Address = "Konya", Description = "bla bla bla", CustomerId=customerId });
+            await _orderWriteRepository.AddAsync(new() { Address = "Ankara", Description = "bla bla bla",CustomerId = customerId });
+            await _orderWriteRepository.SaveAsync();
+
         }
 
         [HttpGet("{id}")]
